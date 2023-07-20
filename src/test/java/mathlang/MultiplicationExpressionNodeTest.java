08897e7d7757e9235a07c24db7ede45f442bed08
g-jensen <greg.micah.jensen@gmail.com>
2023-07-20 19:02:58 -0400

    Added square test

--- src/test/java/mathlang/MultiplicationExpressionNodeTest.java
+++ src/test/java/mathlang/MultiplicationExpressionNodeTest.java
@@ -16,10 +16,12 @@ class MultiplicationExpressionNodeTest {
         BinaryExpressionNode n2 = new MultiplicationExpressionNode(zero, two);
         BinaryExpressionNode n3 = new MultiplicationExpressionNode(two, three);
         BinaryExpressionNode n4 = new MultiplicationExpressionNode(three, nullNode);
+        BinaryExpressionNode n5 = new MultiplicationExpressionNode(two,two);
 
         assertEquals(new Value("0"),n1.evaluate());
         assertEquals(new Value("0"),n2.evaluate());
         assertEquals(new Value("6"),n3.evaluate());
         assertEquals(new NullValue(),n4.evaluate());
+        assertEquals(new Value("4"),n5.evaluate());
     }
 }
