6340c903230c9f2dabf82f238f40b30722c38c0b
g-jensen <greg.micah.jensen@gmail.com>
2023-07-20 15:16:27 -0400

    Updated ExpressionTreeBuilder to use ExpressionNodeFactory

--- src/main/java/mathlang/ExpressionTreeBuilder.java
+++ src/main/java/mathlang/ExpressionTreeBuilder.java
@@ -1,46 +1,26 @@
 package mathlang;
 
-import java.text.NumberFormat;
-import java.text.ParsePosition;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class ExpressionTreeBuilder {
     public BinaryExpressionNode build(String[] tokens) {
+        ExpressionNodeFactory nodeFactory = new ExpressionNodeFactory(this);
         for (int i = 0; i < tokens.length; i++) {
-            String token = tokens[i];
-            if (token.equals("+")) {
-                return buildAdditionExpressionNode(tokens,i);
-            } else if (isNumeric(token)) {
-                return new ConstantExpressionNode(Integer.parseInt(token));
+            BinaryExpressionNode node = nodeFactory.createNode(tokens,i);
+            if (Objects.nonNull(node)) {
+                return node;
             }
         }
         return new NullExpressionNode();
     }
-    // TODO - move to AdditionExpressionNodeFactory
-    private BinaryExpressionNode buildAdditionExpressionNode(String[] tokens, int tokenIndex) {
-        try {
-            String[] p1 = nextParameter(tokens,tokenIndex);
-            String[] p2 = nextParameter(tokens,tokenIndex+p1.length);
-            return new AdditionExpressionNode(build(p1), build(p2));
-        } catch (ArrayIndexOutOfBoundsException e) {
-            return new NullExpressionNode();
-        }
-    }
-    private String[] nextParameter(String[] tokens, int startingIndex) {
-        if (isNumeric(tokens[startingIndex+1])) {
+    public String[] nextParameter(String[] tokens, int startingIndex) {
+        if (Utils.isNumeric(tokens[startingIndex+1])) {
             return new String[]{tokens[startingIndex+1]};
         } else {
             return getNest(tokens, startingIndex);
         }
     }
-
-    // https://stackoverflow.com/questions/1102891/how-to-check-if-a-string-is-numeric-in-java
-    private boolean isNumeric(String str) {
-        ParsePosition pos = new ParsePosition(0);
-        NumberFormat.getInstance().parse(str, pos);
-        return str.length() == pos.getIndex();
-    }
-
     private String[] getNest(String[] tokens, int startingIndex) {
         ArrayList<String> outputTokens = new ArrayList<>();
         int openNestCount= 0;
