2dea7d1003026bec65fc969dbee9385330de4b6c
g-jensen <greg.micah.jensen@gmail.com>
2023-07-21 15:23:20 -0400

    Removed unnecessary default constructor

--- src/main/java/mathlang/expressionnode/BinaryExpressionNode.java
+++ src/main/java/mathlang/expressionnode/BinaryExpressionNode.java
@@ -3,10 +3,6 @@ package mathlang.expressionnode;
 import mathlang.Value;
 
 public abstract class BinaryExpressionNode implements ExpressionNode {
-    public BinaryExpressionNode() {
-        this.left = null;
-        this.right = null;
-    }
     public BinaryExpressionNode(ExpressionNode left, ExpressionNode right) {
         this.left = left;
         this.right = right;
