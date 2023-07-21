339fea17cd888b40465cb46c90bba825c13011ab
g-jensen <greg.micah.jensen@gmail.com>
2023-07-21 15:23:56 -0400

    Changed NullExpressionNode to implement ExpressionNode

--- src/main/java/mathlang/expressionnode/NullExpressionNode.java
+++ src/main/java/mathlang/expressionnode/NullExpressionNode.java
@@ -4,7 +4,7 @@ import mathlang.NullValue;
 import mathlang.Value;
 import mathlang.expressionnode.BinaryExpressionNode;
 
-public class NullExpressionNode extends BinaryExpressionNode {
+public class NullExpressionNode implements ExpressionNode {
     public Value evaluate() {
         return new NullValue();
     }
