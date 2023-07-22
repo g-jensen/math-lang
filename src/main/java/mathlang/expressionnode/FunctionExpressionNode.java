1a53daafad12a1fa05c9bf8c61c0849856bc27da
g-jensen <greg.micah.jensen@gmail.com>
2023-07-21 21:00:25 -0400

    Created FunctionExpressionNode

---
+++ src/main/java/mathlang/expressionnode/FunctionExpressionNode.java
@@ -0,0 +1,41 @@
+package mathlang.expressionnode;
+
+import mathlang.NullValue;
+import mathlang.Value;
+
+public class FunctionExpressionNode implements ExpressionNode {
+    public FunctionExpressionNode(ExpressionNode[] parameters) throws FunctionParameterCountException, SymbolNameException {
+        this.parameters = parameters;
+        if (parameters.length < 3) {
+            throw new FunctionParameterCountException(3,parameters.length);
+        }
+        this.name = parameters[0].evaluate();
+        if (!name.isWord()) {
+            throw new SymbolNameException(name);
+        }
+        this.value = new Value("FunctionExpression: " + name.toString());
+    }
+    @Override
+    public Value evaluate() {
+        return value;
+    }
+    public Value call(ListExpressionNode parameterValues) throws MissingParametersException, MismatchParameterCountException {
+        ListExpressionNode parameterNames;
+        try {
+            parameterNames = (ListExpressionNode) parameters[1];
+        } catch (Exception e) {
+            throw new MissingParametersException(value.toString());
+        }
+        if (parameterValues.values.length != parameterNames.values.length) {
+            throw new MismatchParameterCountException(parameterValues.values.length,parameterNames.values.length);
+        }
+        Value lastValue = new NullValue();
+        for (int i = 2; i < parameters.length; i++) {
+            lastValue = parameters[i].evaluate();
+        }
+        return lastValue;
+    }
+    private final Value value;
+    private final Value name;
+    ExpressionNode[] parameters;
+}
---
+++ src/test/java/mathlang/expressionnode/FunctionExpressionNodeTest.java
@@ -0,0 +1,95 @@
+package mathlang.expressionnode;
+
+import mathlang.Value;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FunctionExpressionNodeTest {
+
+    @Test
+    void throwsParameterCountException() {
+        ExpressionNode[] p1 = new ExpressionNode[]{
+                new ConstantExpressionNode(new Value("greg"))
+        };
+        assertThrows(
+                FunctionParameterCountException.class,
+                ()-> new FunctionExpressionNode(p1)
+        );
+    }
+
+    @Test
+    void throwsFunctionNameException() {
+        ExpressionNode[] p1 = new ExpressionNode[]{
+                new ConstantExpressionNode(new Value("1")),
+                new ConstantExpressionNode(new Value("1")),
+                new ConstantExpressionNode(new Value("1"))
+        };
+        assertThrows(
+                SymbolNameException.class,
+                ()-> new FunctionExpressionNode(p1)
+        );
+    }
+
+    @Test
+    void evaluates() throws SymbolNameException, FunctionParameterCountException {
+        ExpressionNode[] p1 = new ExpressionNode[]{
+                new ConstantExpressionNode(new Value("greg")),
+                new ConstantExpressionNode(new Value("greg")),
+                new ConstantExpressionNode(new Value("1"))
+        };
+        ExpressionNode e = new FunctionExpressionNode(p1);
+        assertEquals(new Value("FunctionExpression: greg"),e.evaluate());
+    }
+
+    @Test
+    void throwsMissingParametersException() throws FunctionParameterCountException, SymbolNameException {
+        ExpressionNode[] p1 = new ExpressionNode[]{
+                new ConstantExpressionNode(new Value("functionName")),
+                new ConstantExpressionNode(new Value("1")),
+                new ConstantExpressionNode(new Value("1"))
+        };
+        FunctionExpressionNode e = new FunctionExpressionNode(p1);
+        assertThrows(MissingParametersException.class,
+                ()->e.call(new ListExpressionNode(new Value[0]))
+        );
+    }
+
+    @Test
+    void throwsMismatchParametersException() throws FunctionParameterCountException, SymbolNameException {
+        Value[] v1 = {new Value("p1")};
+        ExpressionNode[] p1 = new ExpressionNode[]{
+                new ConstantExpressionNode(new Value("functionName")),
+                new ListExpressionNode(v1),
+                new ConstantExpressionNode(new Value("1"))
+        };
+        FunctionExpressionNode e = new FunctionExpressionNode(p1);
+        assertThrows(MismatchParameterCountException.class,
+                ()->e.call(new ListExpressionNode(new Value[0]))
+        );
+    }
+
+    @Test
+    void callsFunctionWithNoParametersOrDefs() throws FunctionParameterCountException, SymbolNameException, MissingParametersException, MismatchParameterCountException {
+        ExpressionNode name = new ConstantExpressionNode(new Value("functionName"));
+        ExpressionNode params = new ListExpressionNode(new Value[0]);
+        ExpressionNode one = new ConstantExpressionNode(new Value("1"));
+
+        ExpressionNode[] p1 = new ExpressionNode[]{
+                name,
+                params,
+                one
+        };
+        FunctionExpressionNode e1 = new FunctionExpressionNode(p1);
+        assertEquals(new Value("1"),e1.call(new ListExpressionNode(new Value[0])));
+
+        ExpressionNode[] p2 = new ExpressionNode[]{
+                name,
+                params,
+                one,
+                new AdditionExpressionNode(one,one)
+        };
+        FunctionExpressionNode e2 = new FunctionExpressionNode(p2);
+        assertEquals(new Value("2"),e2.call(new ListExpressionNode(new Value[0])));
+    }
+}
