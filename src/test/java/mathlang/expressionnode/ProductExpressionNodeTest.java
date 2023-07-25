d2593a62df5638a81fa3d389ad8419aec2c6eff7
g-jensen <greg.micah.jensen@gmail.com>
2023-07-25 08:21:23 -0400

    Fixed bad test

--- src/test/java/mathlang/expressionnode/ProductExpressionNodeTest.java
+++ src/test/java/mathlang/expressionnode/ProductExpressionNodeTest.java
@@ -18,15 +18,15 @@ class ProductExpressionNodeTest {
         ExpressionNode negOne = new ConstantExpressionNode(new Value("-1"));
         ExpressionNode three = new ConstantExpressionNode(new Value("3"));
         ExpressionNode fun = new ConstantExpressionNode(new Value("identity"));
-        ExpressionNode e1 = new SumExpressionNode(one,one,fun);
-        ExpressionNode e2 = new SumExpressionNode(one,negOne,fun);
-        ExpressionNode e3 = new SumExpressionNode(fun,negOne,fun);
-        ExpressionNode e4 = new SumExpressionNode(zero,fun,fun);
-        ExpressionNode e5 = new SumExpressionNode(zero,zero,zero);
-        ExpressionNode e6 = new SumExpressionNode(zero,three,fun);
+        ExpressionNode e1 = new ProductExpressionNode(one,one,fun);
+        ExpressionNode e2 = new ProductExpressionNode(one,negOne,fun);
+        ExpressionNode e3 = new ProductExpressionNode(fun,negOne,fun);
+        ExpressionNode e4 = new ProductExpressionNode(zero,fun,fun);
+        ExpressionNode e5 = new ProductExpressionNode(zero,zero,zero);
+        ExpressionNode e6 = new ProductExpressionNode(one,three,fun);
 
         assertEquals(new Value("1"),e1.evaluate(f.scope));
-        assertEquals(new Value("0"),e2.evaluate(f.scope));
+        assertEquals(new Value("1"),e2.evaluate(f.scope));
         assertEquals(new Value("0"),e3.evaluate(f.scope));
         assertEquals(new Value("0"),e4.evaluate(f.scope));
         assertEquals(new NullValue(),e5.evaluate(f.scope));
