23ab4528877b890217cd7249093dd17b5efa45a7
g-jensen <greg.micah.jensen@gmail.com>
2023-07-25 09:25:35 -0400

    Added support for file input

--- src/main/java/org/Main.java
+++ src/main/java/org/Main.java
@@ -2,10 +2,16 @@ package org;
 
 import mathlang.Repl;
 
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        new Repl(System.in,System.out).loop();
+        if (args.length < 1) {
+            new Repl(System.in,System.out).loop();
+        } else {
+            new Repl(Files.newInputStream(Paths.get(args[0])),System.out).loop();
+        }
     }
 }
