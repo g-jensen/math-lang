0b9c1ca955cbaba0a3d085e35c15dd1092f86ac6
g-jensen <greg.micah.jensen@gmail.com>
2023-07-25 08:45:45 -0400

    Deleted unused class

--- src/main/java/mathlang/expressionnode/SymbolNameException.java
+++
@@ -1,9 +0,0 @@
-package mathlang.expressionnode;
-
-import mathlang.Value;
-
-public class SymbolNameException extends Throwable {
-    public SymbolNameException(Value name) {
-        super("Invalid function name: \"" + name.toString() + "\". Name must be a word");
-    }
-}
